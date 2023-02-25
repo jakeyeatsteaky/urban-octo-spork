05f79b20a667ccad5f9eb29ce37333175bbbd0a1
Jake Baker <jakebaker@Jakes-MBP.attlocal.net>
2023-02-25 15:00:46 -0800

    "center function"

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include "jake_gl.cpp"
 
-#define WIDTH 800
+#define WIDTH 600
 #define HEIGHT 600
 
 static uint32_t pixels[WIDTH * HEIGHT];
@@ -24,12 +24,29 @@ void jgl_fill_rect(uint32_t* pixels, size_t pixels_width, size_t pixels_height, 
         }
     }
 }
+void jgl_fill_rect_center(uint32_t* pixels, size_t pixels_width, size_t pixels_height, int x0, int y0, size_t w, size_t h, uint32_t color)
+{
+    for(int dy = 0; dy < (int) h; ++dy) {
+        int y = (y0 - h/2) + dy;
+        if(0 <= y && y < (int) pixels_height) {
+            for(int dx = 0; dx < (int) w; ++dx) {
+                int x = (x0 - w/2) + dx;
+                if(0 <= x && x < (int) pixels_width) {
+                    pixels[y * pixels_width + x] = color;
+                }
+            }
+
+        }
+    }
+}
 
 int main(void) 
 {
     const char* file_path = "output.ppm";
     jgl_fill(pixels, WIDTH, HEIGHT, 0xFF000000);    // 0xAABBGGRR
-    jgl_fill_rect(pixels, WIDTH, HEIGHT, 10, 10, 100, 100, 0xFFFFFFFF);
+    size_t rect_w = 200;
+    size_t rect_h = 200;
+    jgl_fill_rect(pixels, WIDTH, HEIGHT, WIDTH/2 - (rect_w/2), HEIGHT/2 - (rect_h/2), 100, 100, 0xFFFFFFFF);
     Errno err = jgl_save_to_ppm(pixels, WIDTH, HEIGHT, file_path);
     if(err) {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
