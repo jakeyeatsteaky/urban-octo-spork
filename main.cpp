78602e976209ccf420c7bbbd160378aa9ef92bb7
Jake Baker <jakebaker@Jakes-MBP.attlocal.net>
2023-02-25 14:38:34 -0800

    fill_rect bug

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ static uint32_t pixels[WIDTH * HEIGHT];
 
 void jgl_fill_rect(uint32_t* pixels, size_t pixels_width, size_t pixels_height, int x0, int y0, size_t w, size_t h, uint32_t color)
 {
-    for(int dy = y0; dy < (int) h; ++dy) {
+    for(int dy = 0; dy < (int) h; ++dy) {
         int y = y0 + dy;
         if(0 <= y && y < (int) pixels_height) {
-            for(int dx = x0; dx < (int) w; ++dx) {
+            for(int dx = 0; dx < (int) w; ++dx) {
                 int x = x0 + dx;
                 if(0 <= x && x < (int) pixels_width) {
                     pixels[y * pixels_width + x] = color;
@@ -28,8 +28,8 @@ void jgl_fill_rect(uint32_t* pixels, size_t pixels_width, size_t pixels_height, 
 int main(void) 
 {
     const char* file_path = "output.ppm";
-    jgl_fill(pixels, WIDTH, HEIGHT, 0xFF23F4AC);    // 0xAABBGGRR
-    jgl_fill_rect(pixels, WIDTH, HEIGHT, 100, 100, 150, 150, 0xFFFF0000);
+    jgl_fill(pixels, WIDTH, HEIGHT, 0xFF000000);    // 0xAABBGGRR
+    jgl_fill_rect(pixels, WIDTH, HEIGHT, 10, 10, 100, 100, 0xFFFFFFFF);
     Errno err = jgl_save_to_ppm(pixels, WIDTH, HEIGHT, file_path);
     if(err) {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
