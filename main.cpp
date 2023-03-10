3f4224a58188eeab1c4a899e24740bf04adb98b5
jake.baker0630 <jake.baker0630@gmail.com>
2023-03-09 21:54:15 -0800

    start line

--- main.cpp
+++ main.cpp
@@ -11,6 +11,30 @@
 
 static uint32_t pixels[WIDTH * HEIGHT];
 
+// TODO: lines
+//      https://www.youtube.com/watch?v=LmQKZmQh1ZQ&t=4683s
+//  @ 1:21:03
+
+void jgl_draw_line(uint32_t* pixels, size_t pixelsWidth, size_t pixels_height, int x1, int y1, int x2, int y2, uint32_t color)
+{
+    //                 y = mx + b - find the m and b
+    //       y1 - m * x1 = b
+    //                y2 = mx2 + y1 - mx1
+    //           y2 - y1 = m (x2 - x1)
+    // y2 - y1 / x2 - x1 = m
+    // y1 - m * x1 = b
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    if(dx != 0){
+        int slope = dy / dx;
+        int b = y1 - slope*x1;
+    } else {
+        
+    }
+
+}
+
 int main(void) 
 {
     jgl_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);    // 0xAABBGGRR
